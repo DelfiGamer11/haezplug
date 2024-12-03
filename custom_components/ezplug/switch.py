89b2a0eb1569bdd5c45d7c71f464150406bbbaf2
lele300311 <97394670+DelfiGamer11@users.noreply.github.com>
2024-12-03 21:46:38 +0100

    Update switch.py

--- custom_components/ezplug/switch.py
+++ custom_components/ezplug/switch.py
@@ -158,7 +158,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         _LOGGER.error("Missing email, password configuration")
         return False
     _LOGGER.info(" === === === EZPlug === === ===")
-    _LOGGER.info("Made by: br0kenpixel")
+    _LOGGER.info("Made by: delfigamer11")
     client = EZClient(email, password)
     devs = client.getAllPlugs()
     for dev in devs:
